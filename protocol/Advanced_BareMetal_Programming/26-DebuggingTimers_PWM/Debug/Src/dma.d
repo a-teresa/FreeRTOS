94b9c70d18bf54aa34907925836d6e8bb481293a
teresa <teresa.mdneto@gmail.com>
2024-09-05 23:32:09 +0100

    add DUI0553A

--- protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/Debug/Src/dma.d
+++ protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/Debug/Src/dma.d
@@ -1,27 +1,18 @@
 Src/dma.o: ../Src/dma.c ../Inc/dma.h \
- C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
- C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
- C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/core_cm4.h \
- C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/cmsis_version.h \
- C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/cmsis_compiler.h \
- C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/cmsis_gcc.h \
- C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/mpu_armv7.h \
- C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
-
+ /Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/core_cm4.h \
+ /Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/cmsis_version.h \
+ /Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ /Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ /Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/mpu_armv7.h \
+ /Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
 ../Inc/dma.h:
-
-C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
-
-C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
-
-C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/core_cm4.h:
-
-C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/cmsis_version.h:
-
-C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/cmsis_compiler.h:
-
-C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/cmsis_gcc.h:
-
-C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/mpu_armv7.h:
-
-C:/Users/Gaming\ Laptop\ RTX/Documents/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+/Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+/Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/core_cm4.h:
+/Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/cmsis_version.h:
+/Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/cmsis_compiler.h:
+/Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/cmsis_gcc.h:
+/Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Include/mpu_armv7.h:
+/Users/anateresaneto/Desktop/app/FreeRTOS/protocol/Advanced_BareMetal_Programming/26-DebuggingTimers_PWM/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
